6572f0216fc8e3b155c2f80af25456b7957d890a
Sean Freiburg <frycicle@gmail.com>
2013-04-17 01:01:12 -0500

    made user scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Shpe::Application.routes.draw do
+  resources :users
+
+
+  get "users/new"
+
+  get "users/create"
+
+  get "users/delete"
+
+  get "users/update"
+
+  get "users/index"
+
+  get "users/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
