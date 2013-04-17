73739c1056e5314caca9032c83e94f52a0ef63bf
Sean Freiburg <frycicle@gmail.com>
2013-04-17 00:38:08 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shpe::Application.config.secret_token = '1d0c0a55adf89d1f66c1b472ac90d3eef66832599b663386cde92c73a2f1f6651a83d95cab62bcaf90db618a65aa503cc3c045f973d0160e9a016b2cb2162496'
