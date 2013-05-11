8e1c9a22abdec314136e6ce2428c9a05cbfe8192
Sean Freiburg <frycicle@gmail.com>
2013-05-10 21:23:33 -0500

    added mission

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,4 +16,33 @@ class StaticPagesController < ApplicationController
   def contact
 
   end
+
+  def mission
+
+  end
+
+  def meeting
+
+  end
+
+  def history
+
+  end
+
+  def constitution
+
+  end
+
+  def regioniv
+
+  end
+
+  def national
+
+  end
+
+  def documentary
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/mission', to: 'static_pages#mission'
   
 
   # The priority is based upon order of creation:
