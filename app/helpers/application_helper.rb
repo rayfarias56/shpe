62bb28dcfcff2d3a87601adf3ac5a823c15eb01d
Sean Freiburg <frycicle@gmail.com>
2013-07-22 20:51:58 -0500

    cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,13 @@ module ApplicationHelper
  # def admin?(user)
   #	user.admin
   #end
+
+  def flash_class(level)
+    case level
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error then "alert alert-error"
+      when :alert then "alert alert-error"
+    end
+  end
 end
