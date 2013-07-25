f418faa9e5e0e93c55da747cf5dde5fd0f63bbdb
Sean Freiburg <frycicle@gmail.com>
2013-07-24 21:39:16 -0500

    change some verbage

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,7 +4,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
-    redirect_to root_url, :notice => "Email sent with password reset instructions."
+    redirect_to root_url, :notice => "An email sent with password reset instructions."
   end
 
   def edit
@@ -14,10 +14,10 @@ class PasswordResetsController < ApplicationController
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+      redirect_to new_password_reset_path, :alert => "The password reset has expired."
     elsif @user.update_attributes(params[:user])
       sign_in @user
-      redirect_to @user, :notice => "Password has been reset!"
+      redirect_to @user, :notice => "Your password has been reset!"
     else
       render :edit
     end
