aa7f744e345d7d8f08849da5b9df485ea8529004
Sean Freiburg <frycicle@gmail.com>
2013-07-29 20:04:19 -0500

    changed deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,42 @@
 require "bundler/capistrano"
+#load 'lib/deploy/seed' #include if you need to load seed data with cap deploy:seed
+
+server "50.116.43.128", :app, :web, :db, :primary => true
+set :user, "deployer" # The server's user for deploys
+set :scm_passphrase, "0ok9ij8uh" # The deploy user's password
 
 set :application, "shpe"
 
-set :user, "sean"
 set :use_sudo, false
 
+default_environment["GEM_PATH"] ="/home/deployer/.rbenv/versions/1.9.3-p385/lib/ruby/gems/1.9.1:/home/deployer/.rbenv/shims/ruby"
+default_environment["PATH"] = "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$HOME/.rbenv/versions/1.9.3-p385/lib/ruby/gems/1.9.1:$PATH"
 
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-set :repository,  "git@github.com:seanfreiburg/shpe.git"
+set :deploy_to, "/var/www/production/shpe"
+set :deploy_via, :remote_cache
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+set :scm, "git"
+set :scm_verbose, true
+set :repository, "git@github.com:seanfreiburg/shpe.git"
 set :branch, "master"
-#set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true # Must be set for the password prompt from git to work
 ssh_options[:forward_agent] = true
 
-
-set :deploy_to, "/var/www/apps/#{application}"
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-server "shpe.seanfreiburg.com", :app, :web, :db, :primary => true
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
+# If you need to load seed data. Syntax: cap deploy:seed
+  desc "Reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  task :start do ; end
+  task :stop do ; end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}" #restarts nginx
+  end
 end
-
-after 'deploy:update_code', 'deploy:migrate'
-
-set :keep_releases, 5
-after "deploy:restart", "deploy:cleanup"
-
 
