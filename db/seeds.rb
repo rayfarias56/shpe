5d058268f6d22aa7881f05d9e2bacbc8e019f7f3
Sean Freiburg <frycicle@gmail.com>
2013-07-31 22:25:36 -0500

    change password on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-user = User.new(:name => 'Sean', :password => '12qwas', :password_confirmation => '12qwas', :email => 'frycicle@gmail.com', :gpa => 2.5, :major => 'Computer Science',:uin => 678802721, :phone_number => 2176173798, :grad_date => '2014/05/15')
+user = User.new(:name => 'Sean', :password => '12qwaszx', :password_confirmation => '12qwaszx', :email => 'frycicle@gmail.com', :gpa => 2.5, :major => 'Computer Science',:uin => 678802721, :phone_number => 2176173798, :grad_date => '2014/05/15')
 user.admin = true
 user.save
