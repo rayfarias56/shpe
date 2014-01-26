3e9d519626f411690f9b67e75710aa8a07e11fbb
Sean Freiburg <frycicle@gmail.com>
2014-01-26 13:50:08 -0600

    yeah bitch

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,15 +1,15 @@
 class EventUsersController < ApplicationController
 
   def create
-    user = User.find_by_uin(params[:event_user][:uin])
+    user = User.find_by_uin(params[:uin])
     if user
-      if EventUser.create(user_id: user, event_id: params[:event_user][:event_id])
-        render json: {error: 'You have successfully registered', status: 'success'}
+      if EventUser.create(user_id: user, event_id: params[:event_id])
+        render json: {message: 'You have successfully registered', status: 'success', name: user.name}
       else
-        render json: {error: 'Error registering', status: 'error'}
+        render json: {message: 'Error registering on server', status: 'error'}
       end
     else
-      render json: {error: 'UIN not found', status: 'error'}
+      render json: {message: 'UIN not found', status: 'error'}
     end
   end
 end
