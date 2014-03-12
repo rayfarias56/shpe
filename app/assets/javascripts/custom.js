b66ff9c5a40d210a541ddba330c44bb9475cd810
Brian Hauger <haugerbr@gmail.com>
2014-03-12 00:01:25 -0500

    This doesnt change anything

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
     jQuery('.carousel').carousel();
     $('.btn-navbar').on('click',function(event) {
-    	$('.nav-collapse').collapse('toggle');
+    $('.nav-collapse').collapse('toggle');
     });
 });
