2c94ea449fc525e31fefef3943ee150806b68f8e
Sean Freiburg <frycicle@gmail.com>
2014-03-25 00:26:18 -0500

    fix rollbar code

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,14 +44,12 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}" #restarts nginx
   end
 
-  task :notify_rollbar do
-    on roles(:app) do |h|
-      revision = `git log -n 1 --pretty=format:"%H"`
-      local_user = `whoami`
-      rollbar_token = '6b622b10cd074f69a56d4c8049e13e1c'
-      rails_env = fetch(:rails_env, 'production')
-      execute "curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user} >/dev/null 2>&1", :once => true
-    end
+  task :notify_rollbar, :roles => :app do
+    set :revision, `git log -n 1 --pretty=format:"%H"`
+    set :local_user, `whoami`
+    set :rollbar_token, '6b622b10cd074f69a56d4c8049e13e1c'
+    rails_env = fetch(:rails_env, 'production')
+    run "curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user} >/dev/null 2>&1", :once => true
   end
 
 end
