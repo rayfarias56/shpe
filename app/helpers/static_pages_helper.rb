cc68aff39311ca50ce6f1d58933848f848774cae
Sean Freiburg <frycicle@gmail.com>
2014-09-11 23:25:25 -0700

    updated eboard

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,14 @@
 module StaticPagesHelper
+
+  def exec_image name
+    img = "exec_board/" + name.gsub(" ","_") + ".jpg"
+    options = {size: '200x200'}
+
+    if FileTest.exist?("#{Rails.root}/app/assets/images/#{img}")
+      image_check = image_tag("#{img}",options)
+    else
+      image_check = image_tag("exec_board/blank-person.jpg", options)
+    end
+    image_check
+  end
 end
