ac7736a96600ec870d4f4d3810f9e944fd50cfe2
Raymond Farias <rayfarias56@gmail.com>
2015-12-03 15:15:03 -0600

    Fix mobile drop-down issue: #142

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,3 +4,18 @@ jQuery(document).ready(function () {
     $('.nav-collapse').collapse('toggle');
     });
 });
+
+// Hacky bootstrap mobile drop-down fix: http://stackoverflow.com/a/25041753/3311366
+$(function() {
+  return $("a.dropdown-toggle:not(.multiselect)").click(function(e) {
+    $("ul.dropdown-menu:not(.multiselect-container)").css("display", "none");
+    $(this).next("ul.dropdown-menu").css("display", "block");
+    return e.stopPropagation();
+  });
+});
+
+$(document).on('click click.dropdown.data-api', function(e) {
+  if (!$(e.target).closest('.dropdown, .multiselect-container').length) {
+    return $("ul.dropdown-menu:not(.multiselect-container)").css("display", "none");
+  }
+});
