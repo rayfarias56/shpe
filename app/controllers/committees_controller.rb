b5b70e6cdbe82ecbd0b57f237aafdc515371e282
Santiago <santiago-g95@live.com>
2016-02-25 00:55:12 -0600

    added Committe model and controller

---
+++ app/controllers/committees_controller.rb
@@ -0,0 +1,7 @@
+class CommitteesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ app/models/committee.rb
@@ -0,0 +1,3 @@
+class Committee < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
